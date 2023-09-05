6e7b4e6cedda46bc96d2ca77fc7be686d8ed6175
Jay <92336716+meltonjt@users.noreply.github.com>
2023-09-04 21:05:42 -0400

    Add files via upload

---
+++ Bot-Master/main.py
@@ -0,0 +1,23 @@
+import discord
+from discord.ext import commands
+from core import embed
+from core import db_interactions as di
+
+bot = commands.Bot(command_prefix="b!")
+
+@bot.event
+async def on_ready():
+    print("[+] Bot online.")
+
+# send error message on every error
+@bot.event
+async def on_command_error(ctx, error):
+    await ctx.send(embed=embed.error("Command Error", str(error)))
+
+import os
+for x in os.listdir('./modules'):
+    if x.endswith('.py'):
+        bot.load_extension(f'modules.{x[:-3]}')
+        print("[!] Loaded module: " + x)
+
+bot.run("")
---
+++ Bot-Master/modules/actions.py
@@ -0,0 +1,68 @@
+import discord, asyncio, random, time
+from discord.ext import commands
+from core import db_interactions as di
+from core import embed
+from core import data_interactions as datai
+from pymongo import MongoClient
+
+client = MongoClient("mongodb+srv://business:doUeTyy8tulrIEI8@cluster0.vp1o3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+db = client["businesscord"]
+users = db["users"]
+
+class Actions(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.cooldown(1, 86400, commands.BucketType.user)
+    @commands.command(name="fraud", description="Lets you commit fraud with a change to earn 1/5th of your max income, or to go bankrupt!", )
+    async def fraud(self, ctx):
+        if di.check_if_exists(ctx.author.id):
+            msg = await ctx.send(embed=embed.warning("Are you sure?", "Are you sure you want to commit fraud? You can lose everything!"))
+            
+            for x in ['✅','❎']:
+                await msg.add_reaction(x)
+
+            def check(reaction, user):
+                return user == ctx.author and str(reaction.emoji) in ['✅','❎']
+            
+            try:    
+                reaction, user = await self.bot.wait_for('reaction_add', timeout=30.0, check=check)
+                
+                if reaction == "✅":
+                    try:
+                        user = users.find_one({"id": ctx.author.id})
+                        
+                        fraud_outcome = random.randint(1, 5)
+
+                        if fraud_outcome == 1:
+                            to_add = user["max_income"] / 5
+
+                            users.update_one({"id":ctx.author.id}, {"$inc": {"cash": to_add}})
+                            msg = await ctx.send(embed=embed.allowed("Fraud success", f"You successfully violated the law and committed tax fraude! {to_add} has been added to your balance."))
+
+                            time.sleep(5)
+                            await msg.delete()
+
+                        elif fraud_outcome == 2:
+                            users.delete_one({"id":ctx.author.id})
+                            msg = await ctx.send(embed=embed.no_income("Failed to commit fraud", f"You've been caught committing fraud. Your company has been forced to go bankrupt, and you will have to rebuild your empire."))
+
+                            time.sleep(5)
+                            await msg.delete()
+
+                    except asyncio.TimeoutError:
+                        await msg.delete()
+                        return
+
+                elif reaction == "❎":
+                    await msg.delete()
+
+            except asyncio.TimeoutError:
+                        await msg.delete()
+                        return
+
+        else:
+            return await ctx.send(embed=embed.error("No company", "You first need to create a company with b!register"))
+
+def setup(bot):
+    bot.add_cog(Actions(bot))
